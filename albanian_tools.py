062b95cb49ef965248201411d577944f0e74b230
Bleron Limani <bleroni@gmail.com>
2024-03-25 18:06:46 +0100

    AlbanianTools class

---
+++ albanian_tools.py
@@ -0,0 +1,5 @@
+from functions import integer_to_albanian_text_converter
+
+class AlbanianTools:
+    def integer_to_albanian_text_converter(self, decimal_integer: int):
+        return integer_to_albanian_text_converter(decimal_integer)
--- main.py
+++ main.py
@@ -1,8 +1,12 @@
-from functions import integer_to_albanian_text_converter
+from albanian_tools import AlbanianTools
+
+albanian_tools = AlbanianTools()
+albanian_text = albanian_tools.integer_to_albanian_text_converter(451)
+print(albanian_text)
 
 
 # Testing the examples provided
-print(integer_to_albanian_text_converter(451))
-print(integer_to_albanian_text_converter(19))
-print(integer_to_albanian_text_converter(717))
-print(integer_to_albanian_text_converter(888))
+# print(integer_to_albanian_text_converter(451))
+# print(integer_to_albanian_text_converter(19))
+# print(integer_to_albanian_text_converter(717))
+# print(integer_to_albanian_text_converter(888))
